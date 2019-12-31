f7049d82fdd8cce7142c866fecc4037a540f2446
Mahamuni Vinay <vinaymahamuni22@gmail.com>
2019-12-31 12:28:24 +0530

    Add HTML formatting example

--- src/i18n.js
+++ src/i18n.js
@@ -5,9 +5,11 @@ Vue.use(VueI18n);
 const messages = {
   en: {
     helloWorld: 'Welcome to Your Vue.js App',
+    description: 'For a guide and recipes on how to configure / customize this project, check out the <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.'
   },
   fr: {
-    helloWorld: 'Bienvenue dans votre application Vue.js'
+    helloWorld: 'Bienvenue dans votre application Vue.js',
+    description: 'Pour un guide et des recettes sur la façon de configurer / personnaliser ce projet, consultez la <a href="https://cli.vuejs.org" target="_blank" rel="noopener">documentation vue-cli</a>.'
   },
 };
 
